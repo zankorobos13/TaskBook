1901bbb598d8a6bc091b82db7f0431dc4da781fd
zankorobos13 <aruseniys13@yandex.ru>
2022-03-07 15:49:11 +0300

    Добавьте файлы проекта.

---
+++ TaskBook/TaskBook/Classes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySqlConnector;
+
+namespace TaskBook
+{
+    // Класс для работы с базой данных 
+    public class DB
+    {
+        readonly MySqlConnection connection = new MySqlConnection("server=remotemysql.com;port=3306;username=NqaN7lnsn5;password=WGxtoGYopq;database=NqaN7lnsn5");
+
+        // Открыть соединение
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        // Закрыть соединение
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        // Вернуть соединение
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
